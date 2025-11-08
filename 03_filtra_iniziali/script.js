cc3b131791c72f92b1fb43d9487ded0e3f9865f5
vanessa <vane.busato9@gmail.com>
2025-11-08 10:30:34 +0100

    aggiunto es. 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function parole(nomi, lettera) {
+    const result = [];
+    for (let i = 0; i < nomi.length; i++) {
+        if (nomi[i][0] === lettera) {
+            result.push(nomi[i]);
+        }
+    }
+    return result;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(parole(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
